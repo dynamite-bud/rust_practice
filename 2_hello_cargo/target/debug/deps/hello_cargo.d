5facfa631ccb9f7a906165f05bfd2e89dc07228d
Rudra Arora <aroraru@ITSERV.SCSS.TCD.IE>
2022-09-29 18:14:21 +0100

    2_hello_cargo

---
+++ 2_hello_cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+D:\Projects\Rust_practice\2_hello_cargo\target\debug\deps\hello_cargo.exe: src\main.rs
+
+D:\Projects\Rust_practice\2_hello_cargo\target\debug\deps\hello_cargo.d: src\main.rs
+
+src\main.rs:
---
+++ 2_hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+D:\Projects\Rust_practice\2_hello_cargo\target\debug\hello_cargo.exe: D:\Projects\Rust_practice\2_hello_cargo\src\main.rs
